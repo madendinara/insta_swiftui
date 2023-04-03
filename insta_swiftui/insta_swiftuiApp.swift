a0439f48ba045ec272c9a997716cc574803652f1
Dinara Zimanova <madendinara@gmail.com>
2023-04-02 22:44:41 -0400

    initial commit

---
+++ insta_swiftui/insta_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  insta_swiftuiApp.swift
+//  insta_swiftui
+//
+//  Created by Динара Зиманова on 4/2/23.
+//
+
+import SwiftUI
+
+@main
+struct insta_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
